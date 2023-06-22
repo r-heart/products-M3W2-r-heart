33a5d65b60fdeb6a9eedaf884675ea085e8f9e94
r-heart <rachaelkheart@gmail.com>
2023-06-22 09:56:56 -0700

    feat: :sparkles: render out list of products as shown in Eduflow exercise

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ function App() {
   const products = useProducts();
   console.log(products);
 
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+  <ul className="container mx-auto mt-8 flex flex-col items-center justify-center">{products.map((product) => (
+  <li key={product.id}>
+  {product.name} - {product.price}
+  </li>
+  ))}
+  </ul> 
+  );
 }
 
 export default App;
