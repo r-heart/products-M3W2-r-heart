4f971ec41fe67a23ab0334b3aa98622feab99a62
r-heart <rachaelkheart@gmail.com>
2023-06-21 18:34:17 -0700

    feat: :sparkles: fetch and update products--first task

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,18 @@
+import React from "react";
+
+const BASE_URL = "http://localhost:3001/products";
+
 function App() {
+  const [products, setProducts] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(BASE_URL).then(resp => resp.json()).then(data => {
+      setProducts(data)
+    })
+  }, []);
+
   return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
+
