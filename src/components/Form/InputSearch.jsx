931e227cfaa2fcc928e08dbf81c4db07cbe7a116
r-heart <rachaelkheart@gmail.com>
2023-06-22 20:07:36 -0700

    refactor: :recycle: refactor inputsearch from searchbar into its own component

---
+++ src/components/Form/InputSearch.jsx
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+
+export default function InputSearch({ id, label }) {
+  return (
+    <>
+      <label htmlFor="search" className="sr-only">
+        {label}
+      </label>
+      <input
+        type="search"
+        placeholder={label}
+        id={id}
+        className="border-gray-300mpx-4 focus-border-indigo-500 rounded-md py-2 shadow-sm focus:ring-indigo-500"
+      ></input>
+    </>
+  );
+}
+
+InputSearch.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+};
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,15 +1,9 @@
+import InputSearch from "./Form/InputSearch";
+
 export default function SearchBar() {
   return (
     <form className="my-4 flex flex-col items-center gap-y-4">
-      <label htmlFor="search" className="sr-only">
-        Search
-      </label>
-      <input
-        type="search"
-        placeholder="Search"
-        id="search"
-        className="border-gray-300mpx-4 focus-border-indigo-500 rounded-md py-2 shadow-sm focus:ring-indigo-500"
-      ></input>
+      <InputSearch id="search" label="Search" />
       <label htmlFor="category" className="sr-only">
         Category
       </label>
