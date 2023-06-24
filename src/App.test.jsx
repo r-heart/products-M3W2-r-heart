7047b6ebb94f7d4c05fda7a0c353eafb1ced8a7f
r-heart <rachaelkheart@gmail.com>
2023-06-23 18:12:27 -0700

    test: :white_check_mark: add and pass loads and displays products test

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,8 +1,17 @@
 import { render, screen } from "@testing-library/react";
 import { expect, test } from "vitest";
+import data from "../db.json";
 import App from "./App";
 
 test("renders correctly", () => {
   const appRender = render(<App />);
   expect(appRender).toMatchSnapshot();
+});
+
+it("loads and displays products", async () => {
+  render(<App />);
+
+  const products = await screen.findAllByRole("listitem");
+
+  expect(products).toHaveLength(data.products.length);
 });
