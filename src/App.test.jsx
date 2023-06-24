05012dc760353606e851e8c4443204c415fbd86a
r-heart <rachaelkheart@gmail.com>
2023-06-23 18:28:36 -0700

    test: :white_check_mark: add test and pass search functionality, filters by search term

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { expect, test } from "vitest";
 import data from "../db.json";
 import App from "./App";
@@ -14,4 +15,29 @@ it("loads and displays products", async () => {
   const products = await screen.findAllByRole("listitem");
 
   expect(products).toHaveLength(data.products.length);
+});
+
+describe("🔍 Search", () => {
+  it("filters by search term", async () => {
+    // Arrange
+    const query = "a";
+    const expectedProducts = data.products.filter((product) =>
+      product.name.toLowerCase().includes(query)
+    );
+    const user = userEvent.setup();
+    render(<App />);
+
+    // Act
+    const searchInput = screen.getAllByRole("searchbox");
+
+    // *Make sure that the product list is loaded
+    await screen.findAllByRole("listitem");
+
+    await user.type(searchInput, query);
+
+    const actualProducts = await screen.findAllByRole("listitem");
+
+    // Assert
+    expect(actualProducts).toHaveLength(expectedProducts.length);
+  });
 });
